f1cc31a63fc7faeb4e3705f4d42fcab9ea1fa7f0
mazidong <mazidong@jk.cn>
2018-03-19 23:18:31 +0800

    提交代码

--- TestBroadCast/app/src/main/java/com/example/mazidong/testbroadcast/MainActivity.java
+++ TestBroadCast/app/src/main/java/com/example/mazidong/testbroadcast/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,19 +16,23 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private IntentFilter intentFilter;
-    //private NetWorkChangeReveiver netWorkChangeReveiver;
+    private NetWorkChangeReveiver netWorkChangeReveiver;
+
+    private IntentFilter intentFilter2;
+    private LocalReceiver localReceiver;
+    private LocalBroadcastManager localBroadcastManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*intentFilter = new IntentFilter();
+        intentFilter = new IntentFilter();
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
 
         netWorkChangeReveiver = new NetWorkChangeReveiver();
 
-        registerReceiver(netWorkChangeReveiver, intentFilter);*/
+        registerReceiver(netWorkChangeReveiver, intentFilter);
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -41,16 +46,31 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent("com.example.mazidong.testbroadcast.MY_BROADCAST");
             sendBroadcast(intent);
         });*/
+
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+        Button button1 = (Button) findViewById(R.id.button_local);
+        button1.setOnClickListener(v -> {
+            Intent intent = new Intent("com.example.mazidong.testbroadcast.MY_LOCAL_BROADCAST");
+            localBroadcastManager.sendBroadcast(intent);
+        });
+
+        intentFilter2 = new IntentFilter();
+        intentFilter2.addAction("com.example.mazidong.testbroadcast.MY_LOCAL_BROADCAST");
+        localReceiver = new LocalReceiver();
+
+        localBroadcastManager.registerReceiver(localReceiver, intentFilter2);
     }
 
     @Override
     protected void onDestroy() {
-        //unregisterReceiver(netWorkChangeReveiver);
+        unregisterReceiver(netWorkChangeReveiver);
+
+        localBroadcastManager.unregisterReceiver(localReceiver);
 
         super.onDestroy();
     }
 
-    /*class NetWorkChangeReveiver extends BroadcastReceiver {
+    class NetWorkChangeReveiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
             //Toast.makeText(context, "network changes hahaha", Toast.LENGTH_SHORT).show();
@@ -65,5 +85,12 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT).show();
             }
         }
-    }*/
+    }
+
+    class LocalReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "Receive local broadcast", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
